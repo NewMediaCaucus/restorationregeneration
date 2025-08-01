a611a149701a089ebd7735a61a33e22a169c6f8b
Rob Ray <214191491+KDZU-antisocial@users.noreply.github.com>
2025-07-31 23:49:31 -0700

    add favicons

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -6,6 +6,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $page->title() ?> | restoration/regeneration</title>
 
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="<?= url('assets/icons/apple-touch-icon.png') ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?= url('assets/icons/favicon-32x32.png') ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?= url('assets/icons/favicon-16x16.png') ?>">
+  <link rel="icon" type="image/x-icon" href="<?= url('assets/icons/favicon.ico') ?>">
+  <link rel="manifest" href="<?= url('assets/icons/site.webmanifest') ?>">
+  <meta name="theme-color" content="#000000">
+
   <!-- Preload CSS -->
   <link rel="preload" href="<?= url('assets/css/style.css') ?>" as="style">
   <link rel="stylesheet" href="<?= url('assets/css/style.css') ?>">
